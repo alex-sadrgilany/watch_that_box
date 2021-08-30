cefd0dad3f63e09e1abd19bfd4792d2ad93475a6
AS <asadrgilany7@gmail.com>
2021-08-29 21:02:16 -0400

    initial commit

---
+++ javascript.js
@@ -0,0 +1,38 @@
+document.getElementById("button1").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "250px";
+
+});
+
+
+document.getElementById("button2").addEventListener("click", function(){
+
+    document.getElementById("box").style.backgroundColor = "Blue";
+
+});
+
+
+document.getElementById("button3").addEventListener("click", function(){
+
+    document.getElementById("box").style.opacity = "50%";
+
+});
+
+
+document.getElementById("button4").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "150px";
+    document.getElementById("box").style.opacity = "100%";
+    document.getElementById("box").style.backgroundColor = "Orange";
+    document.getElementById("box").style.border = "none"
+
+});
+
+document.getElementById("button5").addEventListener("click", function(){
+
+    document.getElementById("box").style.border = "thick dotted red";
+
+});
+
+
+style="height:150px; width:150px; background-color:orange; margin:25px"
